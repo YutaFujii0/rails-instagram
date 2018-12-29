ebf3d391e7c65b1ec8af9e4f3e5a47efbb3cd3df
Yuta Fujii <y.f.mar2@gmail.com>
2018-12-29 18:24:00 +0900

    add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages/home', to: 'pages#home'
-  root 'pages#home'
+  root 'posts#index'
   resources :users, only: [:show]
+  resources :posts, only: [:index, :new, :create] do
+    resources :photos, only: [:create]
+  end
 end
